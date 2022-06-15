b239cc2d0da7775b40fef053cdb9f50570841bfe
RohanKetkar <101008728+RohanKetkar@users.noreply.github.com>
2022-06-15 17:51:46 +0530

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import "./style.css"
 const useState = () => {
+    // const initialData = 0;
+    // const [myNum, setMyNum] = React.useState(initialData);
     const [myNum, setMyNum] = React.useState(0);
     return (
         <>
             <div className='center-div'>
                 <p>{myNum}</p>
-                <div className='button2'>
+                <div className='button2' onClick={() =>  setMyNum(myNum +1)}>
                     <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
                     INCR
                 </div>
-                <div className='button2' >
-                    
+                <div className='button2' onClick={() =>myNum >0 ? setMyNum(myNum-1):setMyNum(0)}>
+
                     <span></span>
                     <span></span>
                     <span></span>
@@ -26,3 +28,4 @@ const useState = () => {
     )
 }
 export default useState;
+          
